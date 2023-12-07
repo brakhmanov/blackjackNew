4159a233d3206baacdae4b05f01e43691fd3b51c
brakhmanov <dumorph@gmail.com>
2023-12-07 03:11:26 +0100

    added Player class

--- app/src/main/java/com/example/blackjacknew/Player.kt
+++ app/src/main/java/com/example/blackjacknew/Player.kt
@@ -1,4 +1,35 @@
 package com.example.blackjacknew
 
-class Player {
+class Player(val name: String) {
+    val hand = mutableListOf<Card>()
+
+    fun addToHand(card: Card) {
+        hand.add(card)
+    }
+
+    fun calculateHandValue(): Int {
+        var sum = 0
+        var numAces = 0
+
+        for (card in hand) {
+            when (card.rank) {
+                "A" -> {
+                    sum += 11
+                    numAces++
+                }
+                "K","Q","J" -> sum += 10
+                else -> sum += card.rank.toInt()
+            }
+        }
+        while(sum > 21 && numAces > 0) {
+            sum -= 10
+            numAces--
+        }
+        return sum
+    }
+
+    fun clearHand() {
+        hand.clear()
+    }
+
 }
