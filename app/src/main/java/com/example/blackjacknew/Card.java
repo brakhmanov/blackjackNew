60871721007b86247069276aac88033a1c332928
brakhmanov <dumorph@gmail.com>
2023-12-07 03:20:27 +0100

    added Deck class

---
+++ app/src/main/java/com/example/blackjacknew/Card.java
@@ -0,0 +1,4 @@
+package com.example.blackjacknew;
+
+public class Card {
+}
--- app/src/main/java/com/example/blackjacknew/Deck.kt
+++ app/src/main/java/com/example/blackjacknew/Deck.kt
@@ -1,4 +1,31 @@
 package com.example.blackjacknew
 
 class Deck {
+    private val suits = listOf("Hearts", "Diamonds", "Clubs", "Spades")
+    private val ranks = listOf("2","3","4","5","6","7","8","9","10","J","Q","K","A")
+    private val cards = mutableListOf<Card>()
+
+    init {
+        suits.forEach {
+            suit -> ranks.forEach {
+                rank -> cards.add(Card(suit,rank))
+        }
+        }
+        cards.shuffle()
+    }
+
+    fun drawCard(): Card? {
+        return if (cards.isNotEmpty()) cards.removeAt(0)
+        else null
+    }
+
+    fun reset () {
+        cards.clear()
+        suits.forEach {
+            suit -> ranks.forEach {
+                rank -> cards.add(Card(suit,rank))
+        }
+        }
+        cards.shuffle()
+    }
 }
